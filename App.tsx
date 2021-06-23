8091448c9a4d677a63320eb6da6006c27966d54c
Josileudo Rodrigues <josileudorodrigues75@gmail.com>
2021-06-22 23:24:52 -0300

    add fonts in project and config splash screen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
+
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from '@expo-google-fonts/inter';
+import AppLoading from "expo-app-loading"
+
+import {Inter_400Regular, Inter_500Medium} from '@expo-google-fonts/inter'
+import {Rajdhani_500Medium, Rajdhani_700Bold} from '@expo-google-fonts/rajdhani'
 
 import { Signin } from './src/screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading/>
+  }
+
   return (
     <Signin/>
   );
